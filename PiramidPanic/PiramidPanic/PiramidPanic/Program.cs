414a277acd3621cd4610121cdc70a270e24ae9f8
martijn geurs <marijn.geurs@gmail.com>
2013-11-28 10:49:15 +0100

    naam namespace, class game1, game1.cs gewijzigd in PyramidPanic en logboek veranderd

--- PiramidPanic/PiramidPanic/PiramidPanic/Program.cs
+++ PiramidPanic/PiramidPanic/PiramidPanic/Program.cs
@@ -10,7 +10,7 @@ namespace PiramidPanic
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (PyramidPanic game = new PyramidPanic())
             {
                 game.Run();
             }
